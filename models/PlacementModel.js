26c30b70cca56567c21b52df2c21364a0e1921b2
Kousthubha <krishnakousthubha@gmail.com>
2020-04-01 19:14:59 +0530

    Placement Routes Added

--- models/PlacementModel.js
+++ models/PlacementModel.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
 
 const PlacementSchema = new mongoose.Schema({
-
+    company: { type: String, required: "Required" },
+    job_type: { type: String, required: "Required" }, // Full time or Intern
+    job_description: { type: String, required: "Required" },
+    salary: { type: Number },
+    placement_drive_details: { type: String }, // Salary or Stipend
+    eligibility: {
+        cgpa: { type: String, required: "Required" },
+        backlogs: { type: Boolean, required: "Required" },
+        branch: { type: [String], required: "Required" }
+    }
+    //branch should be array
 });
 module.exports = mongoose.model("placements", PlacementSchema);
--- routes/placementRoutes.js
+++ routes/placementRoutes.js
@@ -1,23 +1,23 @@
 const express = require('express')
 const router = express.Router()
-const UserModel = require('../models/PlacementModel')
+const PlacementModel = require('../models/PlacementModel')
 
 // GET
 router.get("/", async (req, res) => {
-    console.log("Querying Users Data");
+    console.log("Querying Placements Data");
     var query = req.body;
     try {
-        const users = await UserModel.find(query);
-        res.json(users);
+        const places = await PlacementModel.find(query);
+        res.json(places);
     } catch (err) {
         res.json({ message: err.message });
     }
 });
 router.get("/:id", async (req, res) => {
-    console.log("Querying Specific User Data");
+    console.log("Querying Placement Data");
     try {
-        const user = await UserModel.findById(req.params.id);
-        res.json(user);
+        const place = await PlacementModel.findById(req.params.id);
+        res.json(place);
     } catch (err) {
         res.status(404).json({ message: "Id not found." });
     }
@@ -26,18 +26,18 @@ router.get("/:id", async (req, res) => {
 
 // POST
 router.post("/", async (req, res) => {
-    console.log("Adding New User");
+    console.log("Adding New Placement");
     try {
         // Created
-        var newUser = new UserModel(req.body);
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
+        var newPlace = new PlacementModel(req.body);
+        const savedPlace = await newPlace.save();
+        res.status(201).json(savedPlace);
     } catch (err) {
 
         // Error : User Already Exists
         message = err.message
         if (err.code == 11000)
-            message = "User Already Exists !"
+            message = "Placement Already Exists !"
         res.status(400).json({ message: message });
     }
 });
@@ -45,21 +45,21 @@ router.post("/", async (req, res) => {
 
 // PUT
 router.put("/:id", async (req, res) => {
-    console.log("Updating User");
+    console.log("Updating Placement");
     try {
-        var result = await UserModel.updateOne({ _id: req.params.id }, { $set: req.body });
-        const userUpdated = await UserModel.findById(req.params.id);
-        res.json(userUpdated);
+        var result = await PlacementModel.updateOne({ _id: req.params.id }, { $set: req.body });
+        const placeUpdated = await PlacementModel.findById(req.params.id);
+        res.json(placeUpdated);
     } catch (err) {
         // Not Found
         res.status(404).json({ message: "Invalid Id" });
     }
 });
 router.put("/", async (req, res) => {
-    console.log("Updating All Users");
+    console.log("Updating All Placement");
     try {
-        var result = await UserModel.updateOne(req.body.conditions, { $set: req.body.updates });
-        res.json({ message: "All Users Updated" });
+        var result = await PlacementModel.updateOne(req.body.conditions, { $set: req.body.updates });
+        res.json({ message: "All Placements Updated" });
     } catch (err) {
         // Bad Request
         res.status(400).json({ message: err.message });
@@ -69,10 +69,10 @@ router.put("/", async (req, res) => {
 
 // DELETE
 router.delete("/:id", async (req, res) => {
-    console.log("Delete Specific User");
+    console.log("Delete Specific Placement");
     try {
-        await UserModel.deleteOne({ _id: req.params.id })
-        res.json({ message: "User Deleted" });
+        await PlacementModel.deleteOne({ _id: req.params.id })
+        res.json({ message: "Placement Deleted" });
     } catch (err) {
         res.status(404).json({ message: "Id not found." });
     }
